6feac5e4a8305d656bceb9948474c5fc76537262
eoksone <rcifuentes.osorio@gmail.com>
2023-10-22 19:10:16 -0300

    cambios de manu

--- app/views.py
+++ app/views.py
@@ -47,4 +47,55 @@ def agregar_cliente(request):
         'clientes': clientes
     }
 
+<<<<<<< Updated upstream
     return render(request, 'app/histCliente/agregar_cliente.html', data, data1)
+=======
+    return render(request, 'app/histCliente/listar_cliente.html', data)
+
+
+
+def modificar_cliente(request, id):
+
+    cliente = get_object_or_404(Cliente, pk=id)
+
+    data = {
+        'form': ClienteForm(instance = cliente)
+    }
+
+    if request.method == 'POST':
+        formulario = ClienteForm(data=request.POST, instance=cliente)
+        if formulario.is_valid():
+            formulario.save()
+            messages.success(request, "Modificado Correctamente!")
+            return redirect(to="listar_cliente")
+        data["form"] = formulario
+            #data["mensaje"] = "Cliente Modificado Exitosamente!"
+        #else:
+          
+
+    return render(request, 'app/histCliente/modificar_cliente.html', data)
+
+
+
+def eliminar_cliente(request, id):
+    cliente = get_object_or_404(Cliente, pk=id)
+    cliente.delete()
+    messages.success(request, "Eliminado Correctamente!")
+    return redirect(to="listar_cliente")
+
+
+
+
+def login(request):
+    return render(request, 'registration/accounst/login.html')
+
+
+
+
+def maqueta(request):
+    return render(request, 'app/histCliente/maqueta.html')
+
+
+
+
+>>>>>>> Stashed changes
