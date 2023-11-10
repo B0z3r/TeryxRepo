e92732928231771363144df1a7d91377e91987a4
eoksone <rcifuentes.osorio@gmail.com>
2023-11-09 22:44:12 -0300

    test

--- app/static/app/js/script.js
+++ app/static/app/js/script.js
@@ -1,18 +1,47 @@
-function saludarSegunHora() {
-    var fecha = new Date();
-    var hora = fecha.getHours();
+const charts = document.querySelectorAll(".chart");
 
-    var saludo = "";
-    if (hora >= 5 && hora < 12) {
-        saludo = "Buenos días";
-    } else if (hora >= 12 && hora < 18) {
-        saludo = "Buenas tardes";
-    } else {
-        saludo = "Buenas noches";
-    }
+charts.forEach(function (chart) {
+  var ctx = chart.getContext("2d");
+  var myChart = new Chart(ctx, {
+    type: "bar",
+    data: {
+      labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+      datasets: [
+        {
+          label: "# of Votes",
+          data: [12, 19, 3, 5, 2, 3],
+          backgroundColor: [
+            "rgba(255, 99, 132, 0.2)",
+            "rgba(54, 162, 235, 0.2)",
+            "rgba(255, 206, 86, 0.2)",
+            "rgba(75, 192, 192, 0.2)",
+            "rgba(153, 102, 255, 0.2)",
+            "rgba(255, 159, 64, 0.2)",
+          ],
+          borderColor: [
+            "rgba(255, 99, 132, 1)",
+            "rgba(54, 162, 235, 1)",
+            "rgba(255, 206, 86, 1)",
+            "rgba(75, 192, 192, 1)",
+            "rgba(153, 102, 255, 1)",
+            "rgba(255, 159, 64, 1)",
+          ],
+          borderWidth: 1,
+        },
+      ],
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true,
+        },
+      },
+    },
+  });
+});
 
-    document.getElementById("saludo").innerHTML = saludo;
-}
-
-// Llama a la función para que se ejecute cuando la página se carga
-saludarSegunHora();
+$(document).ready(function () {
+  $(".data-table").each(function (_, table) {
+    $(table).DataTable();
+  });
+});
